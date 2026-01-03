7a85f6af8792364103b2ecc1e6da45ba4fdea2dc
Kato-zero <catochibala@gmail.com>
2026-01-03 07:35:10 +0200

    Remove comment in webhook handler
    Removed comment about logging and inspecting the webhook request.

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,4 @@
 export default async function handler(req, res) {
   console.log("Webhook received:", req.body);
-
-  // You’ll log + inspect first, then we’ll store in DB later
   res.status(200).json({ received: true });
 }
